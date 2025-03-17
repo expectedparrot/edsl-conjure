c990763cff63c2935c32c5222ce1983dd45d925c
John Horton <john.joseph.horton@gmail.com>
2025-03-17 09:17:27 -0400

    Remove debug logs from plugin.py

--- conjure/plugin.py
+++ conjure/plugin.py
@@ -1,13 +1,9 @@
 """
 Plugin interface for edsl integration
 """
-import sys
 import pluggy
 from .conjure import Conjure
 from typing import Dict, Any, Optional
-
-# Print debug information to stderr
-print("Initializing conjure plugin", file=sys.stderr)
 
 # Register with the "edsl" plugin system
 hookimpl = pluggy.HookimplMarker("edsl")
@@ -15,31 +11,24 @@ hookimpl = pluggy.HookimplMarker("edsl")
 class ConjurePlugin:
     """Plugin class for edsl integration"""
     
-    def __init__(self):
-        print("ConjurePlugin instance created", file=sys.stderr)
-    
     @hookimpl
     def plugin_name(self):
         """Return the name of the plugin."""
-        print("plugin_name hook called", file=sys.stderr)
         return "Conjure"
     
     @hookimpl
     def get_plugin_methods(self):
         """Return a dictionary of methods provided by this plugin."""
-        print("get_plugin_methods hook called", file=sys.stderr)
         return {}
     
     @hookimpl
     def plugin_description(self):
         """Return a description of the plugin."""
-        print("plugin_description hook called", file=sys.stderr)
         return "A plugin for edsl that converts survey data files into edsl objects"
     
     @hookimpl
     def edsl_plugin(self, plugin_name=None):
         """Return the Conjure class for integration with edsl"""
-        print(f"edsl_plugin hook called with {plugin_name}", file=sys.stderr)
         return Conjure
     
     @hookimpl
@@ -49,11 +38,7 @@ class ConjurePlugin:
         
         This is the key hook that allows Conjure to be available directly in edsl.
         """
-        print("exports_to_namespace hook called", file=sys.stderr)
-        exports = {"Conjure": Conjure}
-        print(f"Exporting: {exports}", file=sys.stderr)
-        return exports
+        return {"Conjure": Conjure}
         
 # Create a plugin instance
 conjure_plugin = ConjurePlugin()
-print("conjure_plugin created", file=sys.stderr)
